23dd43b0b80ae53622a710de6254a0bb17b23649
GNZTMPZ <10210108@bsi.ac.id>
2022-06-06 18:43:42 +0700

    genesis mk2

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
-import logging
 import os
 import sys
 
-import naff
 import sentry_sdk
 from dotenv import load_dotenv
-from naff import (
-    Activity,
-    ActivityType,
-    Client,
-    CommandTypes,
-    Embed,
-    Intents,
-    InteractionContext,
-    OptionTypes,
-    Permissions,
-    PrefixedContext,
-    SlashCommandChoice,
-    Status,
-    check,
-    context_menu,
-    listen,
-    prefixed_command,
-    slash_command,
-    slash_option,
-)
+from naff import (Activity, ActivityType, Embed, Intents, PrefixedContext,
+                  Status, check, listen, prefixed_command)
+from naff.ext.debug_extension import DebugExtension
 
+from core.base import CustomClient
+from core.extensions_loader import load_extensions
+from core.logging import init_logging
 from utilities.checks import *
-
-load_dotenv()
-
-# logging.basicConfig()
-# cls_log = logging.getLogger(naff.const.logger_name)
-# cls_log.setLevel(logging.DEBUG)
 
 # sentry sdk init
 sentry_sdk.init(
@@ -43,96 +21,91 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
 )
 
-client = Client(
-    intents=Intents.ALL,
-    sync_interactions=True,  # sync application commands with discord
-    delete_unused_application_cmds=True,  # Delete commands that arent listed here
-    default_prefix="!",
-    asyncio_debug=False,  # Enable debug mode for asyncio
-    debug_scope=922523614828433419,  # Override the commands scope, and only create them in this guild
-    status=Status.ONLINE,
-    activity=Activity(
-        name="Selamat / Sukses ™️", type=ActivityType.COMPETING
-    ),  # await client.change_presence(status=Status.IDLE, activity=Activity(name="test",type=ActivityType.STREAMING, url="https://www.twitch.tv/saiencevanadium"))
-)
-# during testing, we recommend setting `debug_scope`, this forces your commands to only be registered in the listed guild
 
-# load the Extensions (scale)
-for root, dirs, files in os.walk("scales"):
-    for file in files:
-        if file.endswith(".py") and not file.startswith("__init__"):
-            file = file.removesuffix(".py")
-            path = os.path.join(root, file)
-            python_import_path = path.replace("/", ".").replace("\\", ".")
+if __name__ == "__main__":
+    # load the environmental vars from the .env file
+    load_dotenv()
 
-            # load the scale
-            client.load_extension(python_import_path)
+    # initialise logging
+    init_logging()
 
-
-@prefixed_command(name="load")
-@check(is_owner())
-async def load(ctx: PrefixedContext, scale: str):
-    client.load_extension(f"scales.{scale}")
-    embed = Embed(
-        description=f"<:check:839158727512293406> **{scale}** has been successfully growed!",
-        color=0x00FF00,
+    # create our bot instance
+    bot = CustomClient(
+        intents=Intents.ALL,  # intents are what events we want to receive from discord, `DEFAULT` is usually fine
+        sync_interactions=True,  # sync application commands with discord
+        auto_defer=True,  # automatically deferring interactions
+        delete_unused_application_cmds=True,  # Delete commands that arent listed here
+        default_prefix="!",
+        asyncio_debug=False,  # Enable debug mode for asyncio
+        debug_scope=922523614828433419,  # Override the commands scope, and only create them in this guild
+        status=Status.ONLINE,
+        activity=Activity(
+            name="Selamat / Sukses ™️", type=ActivityType.COMPETING
+        ),  # await client.change_presence(status=Status.IDLE, activity=Activity(name="test",type=ActivityType.STREAMING, url="https://www.twitch.tv/saiencevanadium"))
     )
-    await ctx.send(embed=embed)
 
+    # bot owner only commands
 
-@prefixed_command(name="unload")
-@check(is_owner())
-async def unload(ctx: PrefixedContext, scale: str):
-    client.unload_extension(f"scales.{scale}")
-    embed = Embed(
-        description=f"<:check:839158727512293406> **{scale}** has been successfully ungrowed!",
-        color=0x00FF00,
-    )
-    await ctx.send(embed=embed)
+    @prefixed_command(name="load")
+    @check(is_owner())
+    async def load(ctx: PrefixedContext, scale: str):
+        bot.load_extension(f"scales.{scale}")
+        embed = Embed(
+            description=f"<:check:839158727512293406> **{scale}** has been successfully growed!",
+            color=0x00FF00,
+        )
+        await ctx.send(embed=embed)
 
+    @prefixed_command(name="unload")
+    @check(is_owner())
+    async def unload(ctx: PrefixedContext, scale: str):
+        bot.unload_extension(f"scales.{scale}")
+        embed = Embed(
+            description=f"<:check:839158727512293406> **{scale}** has been successfully ungrowed!",
+            color=0x00FF00,
+        )
+        await ctx.send(embed=embed)
 
-@prefixed_command(name="reload")
-@check(is_owner())
-async def reload(ctx: PrefixedContext, scale: str):
-    client.reload_extension(f"scales.{scale}")
-    embed = Embed(
-        description=f"<:check:839158727512293406> **{scale}** has been successfully regrowed!",
-        color=0x00FF00,
-    )
-    await ctx.send(embed=embed)
+    @prefixed_command(name="reload")
+    @check(is_owner())
+    async def reload(ctx: PrefixedContext, scale: str):
+        bot.reload_extension(f"scales.{scale}")
+        embed = Embed(
+            description=f"<:check:839158727512293406> **{scale}** has been successfully regrowed!",
+            color=0x00FF00,
+        )
+        await ctx.send(embed=embed)
 
+    def restart_program():
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
 
-def restart_program():
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
+    @prefixed_command(name="reboot")
+    @check(is_owner())
+    async def reboot(ctx: PrefixedContext):
+        embed = Embed(
+            description=f":wave: The bot has been Rebooting, Please Wait..",
+            color=0xE74C3C,
+        )
+        await ctx.send(embed=embed)
+        restart_program()
 
+    @prefixed_command(name="shutdown")
+    @check(is_owner())
+    async def shutdown(ctx: PrefixedContext):
+        embed = Embed(
+            description=f":wave: The bot has been Shutdowned, Goodbye World.",
+            color=0xE74C3C,
+        )
+        await ctx.send(embed=embed)
+        await bot.stop()
 
-@prefixed_command(name="reboot")
-@check(is_owner())
-async def reboot(ctx: PrefixedContext):
-    embed = Embed(
-        description=f":wave: The bot has been Rebooting, Please Wait..",
-        color=0xE74C3C,
-    )
-    await ctx.send(embed=embed)
-    restart_program()
+    # load the debug extension if that is wanted
+    if os.getenv("LOAD_DEBUG_COMMANDS") == "true":
+        DebugExtension(bot=bot)
 
+    # load all extensions in the ./extensions folder
+    load_extensions(bot=bot)
 
-@prefixed_command(name="shutdown")
-@check(is_owner())
-async def shutdown(ctx: PrefixedContext):
-    embed = Embed(
-        description=f":wave: The bot has been Shutdowned, Goodbye World.",
-        color=0xE74C3C,
-    )
-    await ctx.send(embed=embed)
-    await client.stop()
-
-
-@listen()  # this decorator tells snek that it needs to listen for the corresponding event, and run this coroutine
-async def on_ready():
-    logging.info("The Bot is Ready")
-    print(f"This bot is Ready to roll and owned by {client.owner}")
-
-
-client.start(os.getenv("BOT_TOKEN"))
+    # start the bot
+    bot.start(os.getenv("DISCORD_TOKEN"))
